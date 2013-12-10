a39239b3e90cebe4bcb85b8c64d39bb1c11feac6
Jiri Spac <Jiri.Spac@ys.cz>
2013-12-10 13:47:54 +0100

    better comments

--- models/vote-count.js
+++ models/vote-count.js
@@ -1,4 +1,5 @@
-module.exports = {   //of this novel's votes
+//use as a partial in documents where you need voting
+module.exports = {   //of this document's votes
     positive: { type: Number, default: 0, min:0},
     negative: { type: Number, default: 0, min:0}
 };
