72d0e242b27b6caad0b4a96abeed1b89dea90f77
Jiri Spac <capajj@gmail.com>
2014-03-10 04:52:24 +0100

    redesigned the users list

--- public/js/directives/zn-user.js
+++ public/js/directives/zn-user.js
@@ -10,12 +10,15 @@ angular.module('zakonomat').directive('znUser', function (MRBackend) {
 			user: '='
 		},
 		link: function (scope, el, attr) {
-			MRBackend.getModel('novelVote').then(function (voteModel) {
-                var voteLQ = voteModel.liveQuery;
+			MRBackend.getModels(['novelVote', 'comment']).then(function (models) {
+                var voteLQ = models.novelVote.liveQuery;
                 var userId = scope.user._id;
                 scope.voteCountLQ = voteLQ().find({owner: userId}).count().exec();
                 scope.positiveVoteCountLQ = voteLQ().find({owner: userId, value: true}).count().exec();
                 scope.negativeVoteCountLQ = voteLQ().find({owner: userId, value: false}).count().exec();
+
+                var commentLQ = models.comment.liveQuery;
+                scope.commentCountLQ = commentLQ().find({owner: userId}).count().exec();
 			});
 
             scope.$watch('user.creation_date', function (newValue) {
