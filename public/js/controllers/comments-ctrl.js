2b3c14784205419dacbe8c131025cd523aa49110
Jiri Spac <capajj@gmail.com>
2014-03-11 03:01:17 +0100

    redesigned root a bit,  user controller now has pagination with url states same as votes controller  comments controller now works with user or novel  bumped to 0.2.2

--- public/js/controllers/comments-ctrl.js
+++ public/js/controllers/comments-ctrl.js
@@ -2,11 +2,26 @@ app.controller('commentsCtrl', function ($scope, models, $location, dialogServic
 
     var commentModel = models.comment;
     $scope.sort = 'vote_count.positive';
-    var root = $location.search().root;
-    if (root) {
-        $scope.commentsLQ = commentModel.liveQuery().find({root: root, reply_on: null}).sort('-vote_count.karma')
-            .populate('owner', 'fb.username fb.picture.data.url').exec();
+    $scope.owner = $location.search().owner;
+    $scope.root = $location.search().root;
 
+
+    if ($scope.root) {
+        $scope.commentsLQ = commentModel.liveQuery().find({root: $scope.root, reply_on: null}).sort('-vote_count.karma')
+            .populate('owner', 'fb.username fb.picture.data.url').exec();
+        models.novel.query().findOne({_id: $scope.root}).exec().then(function (n) {
+            $scope.novel = n;
+        });
+
+    } else if($scope.owner) {
+        $scope.commentsLQ = commentModel.liveQuery().find({owner: $scope.owner, reply_on: null}).sort('-vote_count.karma')
+            .populate('owner', 'fb.username fb.picture.data.url').exec();
+        models.user.query().findOne({_id: $scope.owner}).exec().then(function (user) {
+            $scope.user = user;
+        });
+    }
+
+    if ($scope.commentsLQ) {
         $scope.showNewCommentInput = function () {
             $scope.newComment = {};
         };
@@ -19,7 +34,7 @@ app.controller('commentsCtrl', function ($scope, models, $location, dialogServic
         };
         $scope.cancelComment = function () {
             $scope.newComment = null;
-        }
+        };
 
     }
 
--- public/js/controllers/users-ctrl.js
+++ public/js/controllers/users-ctrl.js
@@ -1,11 +1,33 @@
 app.controller('usersCtrl', function ($scope, $location, user) {
     var liveQuery = user.liveQuery;
-    var limit = $location.search().limit || 50;
 
-    $scope.LQ = liveQuery().limit(limit).exec();
-    $scope.usersCountLQ = liveQuery().count().exec();
-    $scope.LQ.promise.then(function (LQ) {
-        console.log(LQ);
-    });
+    var runQueries = function () {
+        var limit = $location.search().limit || $scope.pagination.limit;
+
+        $scope.LQ = liveQuery().limit(limit).skip($scope.pagination.skip).exec();
+        $scope.userCountLQ = liveQuery().count().exec();
+        $scope.LQ.promise.then(function (LQ) {
+            console.log(LQ);
+        });
+    };
+    var pag = $location.search().pagination;
+    if (pag) {
+        pag = JSON.parse(pag)
+    }
+
+    $scope.pagination = pag ||  {   //default values
+        page: 1,
+        limit: 15,
+        skip: 0
+    };
+    $scope.pagination.onSelect = function (page) {
+        var pagin = $scope.pagination;
+        pagin.page = page;
+        pagin.skip = (page - 1) * pagin.limit;
+        $location.search('pagination', JSON.stringify(pagin));  //triggers routeUpdate event
+    };
+
+    $scope.$on('$routeUpdate', runQueries);
+    runQueries();
 
 });
